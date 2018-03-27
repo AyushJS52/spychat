f8df294240352605d006287bfa765408fd333d0d
AyushJS52 <ayushjaiswal411@gmail.com>
2018-03-27 21:58:22 +0530

    3 module

--- Spy_details.py
+++ Spy_details.py
@@ -1,6 +1,8 @@
-print ("Hello Default user !")
 
-spy = raw_input("what is your spy name:? ")
-salutation = raw_input("What should we call you Mr or Ms ?")
-spyname = salutation +" "+spy
-print ("Alright "+ spyname +" I'd like to know little bit more about you")
+# default status
+current_status_message = None
+# list of default status
+STATUS_MESSAGES = ["Hi, I'm Ayush", "MRIU"]
+
+spy_name = "Ayush"
+spy_salutation = "Mr."
--- main.py
+++ main.py
@@ -1,50 +1,80 @@
-print ("Hello !")
+from menu import start_chat
 
-spy = raw_input("what is your spy name:? ")
-salutation = raw_input("What should we call you Mr or Ms ?")
-spyname = salutation +" "+spy
-print ("Alright "+ spyname +" I'd like to know little bit more about you")
-# default user or create a new user
-user = raw_input("Would you like to continue with the default user or create a new one (Default or Create) ?")
-# for existing user
-if user == "Default" or user == "default":
-        import Spy_details
+print("WELCOME TO SPYCHAT")
 
-# for a new user
-elif user == "Create" or user == "create":
-        # asking names
-        spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
-        # checking length of the name
-        if len(spy_name) > 0:
-            # asking for salutation
-            spy_salutation = raw_input("What would you like us to call you (Mr. or Ms.) ?")
+spy_name = raw_input("Whats your spy name")
 
-            print("Alright " + spy_salutation + " " + spy_name + " I'd like to know a little bit more about you...")
-            # checking age
-            spy_age = int(raw_input("What's your age?"))
-            if 12 < spy_age < 50:
-                # checking rating
-                spy_rating = float(raw_input("What is your spy rating?"))
-                if spy_rating > 4.5:
-                    print("Outstanding!")
-                elif 3.5 < spy_rating <= 4.5:
-                    print("Amazing!")
-                elif 2.5 <= spy_rating <= 3.5:
-                    print("You can improve!")
-                else:
-                    print("We can help you!")
-                # if spy is online
-                spy_is_online = True
-                # welcome with details
+if len(spy_name) > 0:
 
-                print("Welcome "+spy_salutation+" "+spy_name+" your age is "+str(spy_age)+" and rating is "+str(spy_rating)+"!")
-                print("Proud to have you on board!")
-            # check age
-            else:
-                print("Sorry you are not of the correct age to be a spy")
-        # name not provided
-        else:
-            print("Please provide us with your name first. Try again please.")
-# entry not valid
+    spy_salutation = raw_input("What should we call you (Mr. or Ms.)?")  ##taking the salutation
+
+    spy_name = spy_salutation + "" + spy_name #concatenating
+
+    print("Alright " + spy_name + " I'd like to know a little bit more about you...")
+
 else:
-        print("Please enter default or create.")
+    print("Please provide us with your name first!")
+    # valid entry
+
+
+
+
+print("WELCOME TO SPYCHAT")
+
+default_user = raw_input("Do you wish to continue with the default user(Y or N)? ,If No create a new user")
+
+# press 'Y' for the default user & 'N' to create a new user #
+
+# for default user #
+if default_user == 'Y'or default_user == 'y':
+    print("continue")
+    import spy_details    ##import the spy details from another spy_details file #
+
+    print("Welcome to the spy chat " + spy_salutation + " " + spy_name) ### printing details of a default user####
+    print("Alright " + spy_salutation + " " + spy_name + " I'd like to know a little bit more about you...")
+
+
+# for a new user #
+elif default_user == 'N' or default_user == 'n':
+    # asking new spy name
+    spy_name = raw_input("What's your name?")
+    # checking length of the name
+    if len(spy_name) > 0:
+        #welcome new user
+        print("Welcome "+spy_name+" Glab to have you with us")
+        # asking for spy salutation
+        spy_salutation = raw_input("What would you like us to call you (Mr., Ms. or Mrs.) ?")
+        # welcoming spy
+        print("Welcome to the spy chat " + spy_salutation + " " + spy_name)
+        print("Alright " + spy_salutation + " " + spy_name + " I'd like to know a little bit more about you...")
+    # name not provided
+    else:
+        print("Please provide us with your name first!")
+        # valid entry
+else:
+    print("Please enter default or create.")
+        # checking spy age
+spy_age = int(raw_input("What's your age?"))
+if spy_age > 12 and spy_age < 50:
+    # checking spy rating
+    spy_rating = float(raw_input("What is your spy rating?"))
+    if spy_rating > 4.5:
+        print("Outstanding!")
+    elif spy_rating > 3.5 and spy_rating <= 4.5:
+        print("Amazing!")
+    elif spy_rating >= 2.5 and spy_rating <= 3.5:
+        print("You can surely improve!")
+    else:
+        print("Don't Worry! We'll help you!")
+        # if spy is online
+else:
+    print("You are not ready to be a spy yet!")
+            # name not provided
+
+spy_is_online = True
+            # welcome with details
+print("Authentication complete. Welcome " + spy_name + " age: " + str(spy_age) + " and rating of: " + str(spy_rating) + " Proud to have you onboard")
+        # age is not eligible
+
+#starting the chat application
+start_chat()
